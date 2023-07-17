aae5329c8be7912658b1dd46c97b0c0e14686880
duynhanf <duynhanf@gmail.com>
2023-07-17 09:50:02 +0900

    feat: add more pages

--- packages/sr-frontend/pages/blogs/index.vue
+++ packages/sr-frontend/pages/blogs/index.vue
@@ -1,11 +1,61 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="i in 10" :key="i">
-        <NuxtLink :to="`/posts/${i}`">
-          Post #{{ i }}
-        </NuxtLink>
-      </li>
-    </ul>
+  <div class="blog">
+    <div class="m-container">
+      <div class="blog-main">
+        <div class="blog-main-title">
+          <h2>MIOTO Blog</h2>
+        </div><div class="blog-place">
+          <div class="blog-place-main">
+            <div class="blog-inner">
+              <a href="/blog/kinh-nghiem-lai-xe-o-to-cho-tai-moi--huong-dan-va-loi-khuyen-de-lai-xe-an-toan/CGPYXNA7"><img src="https://n1-cstg.mioto.vn/g/2023/06/13/00/3GWTMTCU.jpg" alt=""></a>
+            </div><div class="group-tag">
+              <span class="info info-bg">Trải nghiệm</span><a href="/blog/kinh-nghiem-lai-xe-o-to-cho-tai-moi--huong-dan-va-loi-khuyen-de-lai-xe-an-toan/CGPYXNA7"><h5>Kinh nghiệm lái xe ô tô cho tài mới: Hướng dẫn và lời khuyên để lái xe an toàn </h5></a>
+            </div><p>Từ khi ô tô trở thành phương tiện giao thông thông dụng, việc sở hữu bằng lái xe ô tô đã trở thành một kỹ năng quan trọng đối với nhiều người. Tuy nhiên, việc lái xe ô tô không chỉ đơn giản là ngồi vào ghế lái và lái đi. Đối với những tài xế mới, có một số kinh nghiệm quan trọng cần được lưu ý để đảm bảo an toàn và tránh những rủi ro trong quá trình lái xe. Trong bài viết này, hãy cùng Mioto tìm hiểu về những kinh nghiệm quan trọng nhất cho tài mới khi lái xe ô tô.</p>
+          </div><div class="blog-place-other">
+            <a class="blog-other-item" href="/blog/thue-xe-o-to-tu-lai-o-tan-binh--su-lua-chon-thong-minh-cho-chuyen-di-hoan-hao/CEV13DIK"><img src="https://n1-cstg.mioto.vn/g/2023/06/03/09/G9DYPJS5.jpg" alt=""><div class="content"><div class="group-tag"><span class="info info-bg">Trải nghiệm</span></div><h6>Thuê Xe Ô Tô Tự Lái ở Tân Bình: Sự Lựa Chọn Thông Minh Cho Chuyến Đi Hoàn Hảo</h6><p class="disc">Khi bạn có kế hoạch du lịch hoặc cần di chuyển trong khu vực thành phố Hồ Chí Minh, một trong những giải pháp tiện lợi và linh hoạt nhất là thuê xe ô tô tự lái. Đặc biệt, ở khu vực Tân Bình, một điểm nổi bật mà Mioto cung cấp dịch vụ thuê xe ô tô tự lái chất lượng và đáng tin cậy. Và khi thuê xe chúng ta có những lợi ích gì? Cùng Mioto khám phá lợi ích của việc thuê xe ô tô tự lái ở Tân Bình và đưa ra một số gợi ý để bạn có một chuyến đi hoàn hảo.</p></div></a><a class="blog-other-item" href="/blog/tu-do-kham-pha-thanh-pho-khi-thue-xe-o-to-tu-lai-tai-quan-binh-thanh/CD12SIL5"><img src="https://n1-tstg.mioto.vn/g/2023/05/05/09/TSY54_MLt_laQvdwDGk3fQ.jpg" alt=""><div class="content"><div class="group-tag"><span class="info info-bg">Trải nghiệm</span></div><h6>Tự do khám phá thành phố khi thuê xe ô tô tự lái tại Quận Bình Thạnh&nbsp;</h6><p class="disc">Quận Bình Thạnh, nằm ở trung tâm Thành phố Hồ Chí Minh, không chỉ là một trong những quận phát triển nhanh nhất, mà còn là một điểm đến hấp dẫn với nhiều điểm du lịch, trung tâm mua sắm và những quán ăn nổi tiếng. Để tận hưởng một chuyến đi tuyệt vời đến quận Bình Thạnh, dịch vụ thuê xe ô tô tự lái là lựa chọn tuyệt vời để bạn có thể thoải mái khám phá và tận hưởng mọi góc cạnh của quận này.</p></div></a><a class="blog-other-item" href="/blog/du-lich-3-mien-cung-mioto/CBGGBXUG"><img src="https://n1-tstg.mioto.vn/g/2023/03/29/09/RaJDNmXOsnc0NfWqqCXZWA.jpg" alt=""><div class="content"><div class="group-tag"><span class="info info-bg">Trải nghiệm</span></div><h6>DU LỊCH 3 MIỀN CÙNG MIOTO</h6><p class="disc">Việt Nam mảnh đất thân thương hình chữ S, với những cảnh sắc thiên nhiên phong phú. Không chỉ vậy với bàn tay kiến tạo của con người cũng đã góp phần tạo nên những công trình kiến trúc độc đáo, và cảnh đẹp cũng có thể là những hình ảnh đời thường của những con người giản dị chân chất mang đậm nét truyền thống. Cùng Mioto du lịch 3 miền để chọn cho mình một địa điểm cho dịp hè này thêm lý tưởng!</p></div></a>
+          </div>
+        </div><div class="blog-content">
+          <div class="blog-content-inner col3-mg20">
+            <a class="item" href="/blog/kinh-nghiem-lai-xe-o-to-cho-tai-moi--huong-dan-va-loi-khuyen-de-lai-xe-an-toan/CGPYXNA7"><div class="fix-img"><img src="https://n1-cstg.mioto.vn/g/2023/06/13/00/3GWTMTCU.jpg" alt=""></div><h5>Kinh nghiệm lái xe ô tô cho tài mới: Hướng dẫn và lời khuyên để lái xe an toàn </h5><p class="disc">Từ khi ô tô trở thành phương tiện giao thông thông dụng, việc sở hữu bằng lái xe ô tô đã trở thành một kỹ năng quan trọng đối với nhiều người. Tuy nhiên, việc lái xe ô tô không chỉ đơn giản là ngồi vào ghế lái và lái đi. Đối với những tài xế mới, có một số kinh nghiệm quan trọng cần được lưu ý để đảm bảo an toàn và tránh những rủi ro trong quá trình lái xe. Trong bài viết này, hãy cùng Mioto tìm hiểu về những kinh nghiệm quan trọng nhất cho tài mới khi lái xe ô tô.</p></a><a class="item" href="/blog/thue-xe-o-to-tu-lai-o-tan-binh--su-lua-chon-thong-minh-cho-chuyen-di-hoan-hao/CEV13DIK"><div class="fix-img"><img src="https://n1-cstg.mioto.vn/g/2023/06/03/09/G9DYPJS5.jpg" alt=""></div><h5>Thuê Xe Ô Tô Tự Lái ở Tân Bình: Sự Lựa Chọn Thông Minh Cho Chuyến Đi Hoàn Hảo</h5><p class="disc">Khi bạn có kế hoạch du lịch hoặc cần di chuyển trong khu vực thành phố Hồ Chí Minh, một trong những giải pháp tiện lợi và linh hoạt nhất là thuê xe ô tô tự lái. Đặc biệt, ở khu vực Tân Bình, một điểm nổi bật mà Mioto cung cấp dịch vụ thuê xe ô tô tự lái chất lượng và đáng tin cậy. Và khi thuê xe chúng ta có những lợi ích gì? Cùng Mioto khám phá lợi ích của việc thuê xe ô tô tự lái ở Tân Bình và đưa ra một số gợi ý để bạn có một chuyến đi hoàn hảo.</p></a><a class="item" href="/blog/tu-do-kham-pha-thanh-pho-khi-thue-xe-o-to-tu-lai-tai-quan-binh-thanh/CD12SIL5"><div class="fix-img"><img src="https://n1-tstg.mioto.vn/g/2023/05/05/09/TSY54_MLt_laQvdwDGk3fQ.jpg" alt=""></div><h5>Tự do khám phá thành phố khi thuê xe ô tô tự lái tại Quận Bình Thạnh&nbsp;</h5><p class="disc">Quận Bình Thạnh, nằm ở trung tâm Thành phố Hồ Chí Minh, không chỉ là một trong những quận phát triển nhanh nhất, mà còn là một điểm đến hấp dẫn với nhiều điểm du lịch, trung tâm mua sắm và những quán ăn nổi tiếng. Để tận hưởng một chuyến đi tuyệt vời đến quận Bình Thạnh, dịch vụ thuê xe ô tô tự lái là lựa chọn tuyệt vời để bạn có thể thoải mái khám phá và tận hưởng mọi góc cạnh của quận này.</p></a><a class="item" href="/blog/du-lich-3-mien-cung-mioto/CBGGBXUG"><div class="fix-img"><img src="https://n1-tstg.mioto.vn/g/2023/03/29/09/RaJDNmXOsnc0NfWqqCXZWA.jpg" alt=""></div><h5>DU LỊCH 3 MIỀN CÙNG MIOTO</h5><p class="disc">Việt Nam mảnh đất thân thương hình chữ S, với những cảnh sắc thiên nhiên phong phú. Không chỉ vậy với bàn tay kiến tạo của con người cũng đã góp phần tạo nên những công trình kiến trúc độc đáo, và cảnh đẹp cũng có thể là những hình ảnh đời thường của những con người giản dị chân chất mang đậm nét truyền thống. Cùng Mioto du lịch 3 miền để chọn cho mình một địa điểm cho dịp hè này thêm lý tưởng!</p></a><a class="item" href="/blog/dong-xe-o-to-phu-hop-de-di-cam-trai/1YLIN2Y3"><div class="fix-img"><img src="https://n1-tstg.mioto.vn/g/2023/03/20/17/scq_Wr2ayNKLbPQFzivXxw.jpg" alt=""></div><h5>DÒNG XE Ô TÔ PHÙ HỢP ĐỂ ĐI CẮM TRẠI</h5><p class="disc">Đi cắm trại là hoạt động giải trí rất phổ biến và được nhiều người yêu thích. Tuy nhiên, để tận hưởng hoàn toàn những trải nghiệm đó, bạn cần phải chuẩn bị đầy đủ các trang thiết bị và đồ dùng cần thiết. Và một trong những yếu tố quan trọng không thể thiếu đó là chiếc xe phù hợp để đi cắm trại. Trong bài viết này, chúng ta sẽ tìm hiểu về những dòng xe phù hợp để đi cắm trại.</p></a><a class="item" href="/blog/nhung-meo-vat-giup-o-to-luon-ben-bi/1V8KVSHC"><div class="fix-img"><img src="https://n1-tstg.mioto.vn/g/2023/04/13/14/XbVj9-WHER0p7nU0bsAMJQ.jpg" alt=""></div><h5>NHỮNG MẸO VẶT GIÚP Ô TÔ LUÔN BỀN BỈ</h5><p class="disc"> Xe ô tô là một trong những công cụ di chuyển hàng ngày của chúng ta, và việc giữ cho chiếc xe luôn bền bỉ là rất quan trọng. Bên cạnh việc nhà cung cấp sản phẩm chất lượng thì việc bảo quản ô tô luôn hoạt động tốt còn phụ thuộc rất nhiều vào cách sử dụng của chủ sở hữu. Bảo dưỡng chiếc xe tốt là một trong những việc quan trọng nhất để giúp xe hoạt động bền bỉ và an toàn trong suốt quá trình sử dụng.   Để giúp bạn giữ cho xe của mình hoạt động tốt và luôn sẵn sàng cho các chuyến đi, hãy cùng Mioto điểm qua các mẹo vặt giúp “xế hộp” của bạn luôn bền bỉ.</p></a><a class="item" href="/blog/nhung-nha-tho-ly-tuong-cho-mua-le-giang-sinh/19WNIZ51"><div class="fix-img"><img src="https://n1-cstg.mioto.vn/g/2023/06/14/15/3P1J6Y2Y.jpg" alt=""></div><h5> NHỮNG NHÀ THỜ LÝ TƯỞNG CHO MÙA LỄ GIÁNG SINH </h5><p class="disc">Giáng Sinh là dịp tuyệt vời để bạn tạm quên đi những lo toan, bộn bề cuộc sống và dành thời gian quý báu cho những người thân yêu.  Nếu bạn đang tìm kiếm một địa điểm để đón Giáng Sinh trong năm nay, hãy để Mioto gợi ý cho bạn những điểm đến thú vị giúp bạn tận hưởng trọn vẹn không khí ấm áp cho mùa lễ hội cuối năm.</p></a><a class="item" href="/blog/nhung-dia-diem-du-lich-trong-thang-12/1RCQ8MSX"><div class="fix-img"><img src="https://n1-cstg.mioto.vn/g/2022/10/26/14/78I7MW7E.jpg" alt=""></div><h5>NHỮNG ĐỊA ĐIỂM DU LỊCH TRONG THÁNG 12</h5><p class="disc">Tháng 12 với khí hậu dễ chịu và cảnh sắc vào đông tuyệt đẹp chính là thời điểm thích hợp du khách lên kế hoạch và tham quan các địa điểm nổi tiếng. Nếu bạn đang tìm kiếm một số điểm đến lý tưởng trong tháng 12, dưới đây là một số gợi ý khá lý tưởng cho chuyến đi của bạn.&nbsp;</p></a><a class="item" href="/blog/nen-mua-xe-chay-xang-hay-dau/1PH9E9VW"><div class="fix-img"><img src="https://n1-cstg.mioto.vn/g/2022/09/28/23/6JRP8FG3.jpg" alt=""></div><h5>NÊN MUA XE CHẠY XĂNG HAY DẦU?</h5><p class="disc">Trên thị trường hiện nay có rất nhiều loại ô tô khác nhau với hai lựa chọn động cơ. Điều này khiến khách hàng băn khoăn không biết nên mua ô tô chạy xăng hay chạy dầu trong bối cảnh khủng hoảng năng lượng hiện nay.</p></a><a class="item" href="/blog/kinh-nghiem-thue-xe-du-lich-nhan-dip-le-29/22U7VG8P"><div class="fix-img"><img src="https://n1-cstg.mioto.vn/g/2022/07/28/21/GCT98FDH.jpg" alt=""></div><h5>KINH NGHIỆM THUÊ XE DU LỊCH NHÂN DỊP LỄ 2/9</h5><p class="disc">Ngày 2/9 là ngày nghỉ lễ quốc khánh nên rất nhiều người thuê xe đi du lịch trong giai đoạn này. Điều này dẫn đến nhu cầu thuê xe ngày càng tăng nhanh. Do đó, việc thuê xe có thể trở nên phức tạp và khó khăn hơn. Ở bài viết này, Mioto xin đưa ra một số kinh nghiệm quan trọng mà bạn nên biết khi thuê xe trong dịp lễ.</p></a>
+          </div>
+        </div><div class="page-numbers">
+          <div class="wrap-svg">
+            <svg width="6" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.41992 1.07568L0.759103 6.7365L6.41992 12.3973" stroke="#060606" stroke-width="1.4152" stroke-linecap="round" stroke-linejoin="round" /></svg>
+          </div><p class="number active">
+            1
+          </p><p class="number ">
+            2
+          </p><p class="number ">
+            3
+          </p><p class="number ">
+            4
+          </p><p class="number ">
+            5
+          </p><p class="number ">
+            6
+          </p><p class="number ">
+            7
+          </p><p class="number ">
+            8
+          </p><p class="number ">
+            9
+          </p><p class="number ">
+            10
+          </p><p class="number ">
+            11
+          </p><p class="number ">
+            12
+          </p><p class="number ">
+            13
+          </p><p class="number ">
+            14
+          </p><p class="number ">
+            15
+          </p><div class="wrap-svg">
+            <svg width="6" height="12" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.57812 1.07568L7.23894 6.7365L1.57813 12.3973" stroke="#060606" stroke-width="1.4152" stroke-linecap="round" stroke-linejoin="round" /></svg>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
---
+++ packages/sr-frontend/pages/howitwork.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    howitwork
+  </div>
+</template>
